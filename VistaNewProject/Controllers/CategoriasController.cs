069f9bbc9065ff0771ddbb750761adfb748ca3b9
Aska0915 <estebantorr3ss@gmail.com>
2024-04-04 19:11:41 -0500

    Categoria js,Vista,Controladores 40%

---
+++ VistaNewProject/Controllers/CategoriasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VistaNewProject.Services;
+
+namespace VistaNewProject.Controllers
+{
+    public class CategoriasController : Controller
+    {
+
+        private readonly IApiClient _client;
+        public CategoriasController(IApiClient client)
+        {
+            _client = client;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categorias = await _client.GetCategoriaAsync();
+
+            if (categorias == null)
+            {
+                return NotFound("error");
+            }
+            return View(categorias);
+        }
+    }
+}
