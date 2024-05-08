2f9cc7bfde18021627300bdf45b2db118ddba60a
Oscar Alejandro <oarincon517@misena.edu.co>
2024-05-08 17:07:09 -0500

    Dashboard

---
+++ VistaNewProject/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿namespace VistaNewProject.Models
+{
+    public class Home
+    {
+        public List<Producto> Productos { get; set; }
+        public List<Detallepedido> detallepedidos { get; set; }
+        public List<Compra> Compras { get; set; }
+        public List<Proveedor> Proveedores { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public List<Pedido> Pedidos { get; set; }
+         
+        public List<Lote> Loteres { get; set; }
+    }
+}
